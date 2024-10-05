86ca48d3f32b3d59ce240174928ada33d54b025e
Hung Mai <126455648+mhhung0811@users.noreply.github.com>
2024-10-05 12:53:55 +0700

    feat(discount api): create discount api

--- main/src/routes/discount.route.ts
+++ main/src/routes/discount.route.ts
@@ -1,5 +1,19 @@
+import { PERMISSIONS } from '@/constants/permission.constants';
+import { discountController } from '@/container/discount.container';
+import { CreateDiscountReq } from '@/dto/discount/create-discount.req';
+import { authenticateJWT } from '@/middleware/authenticate.middleware';
+import { checkPermission } from '@/middleware/check-permission.middleware';
+import { classValidate } from '@/middleware/class-validate.middleware';
 import express from 'express';
 
 const discountRouter = express.Router();
+
+discountRouter.post(
+  '/create',
+  authenticateJWT,
+  checkPermission([PERMISSIONS.MANAGE_EMPLOYEE]),
+  classValidate(CreateDiscountReq),
+  discountController.create.bind(discountController)
+);
 
 export default discountRouter;
