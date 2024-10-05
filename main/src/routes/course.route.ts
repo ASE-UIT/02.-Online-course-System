4d4eaec18a6340e38a5a059c1a399de5c6562ae6
Hung Mai <126455648+mhhung0811@users.noreply.github.com>
2024-10-05 14:21:01 +0700

    feat(course api): soft-delete course api

--- main/src/routes/course.route.ts
+++ main/src/routes/course.route.ts
@@ -1,5 +1,16 @@
+import { PERMISSIONS } from '@/constants/permission.constants';
+import { courseController } from '@/container/course.container';
+import { authenticateJWT } from '@/middleware/authenticate.middleware';
+import { checkPermission } from '@/middleware/check-permission.middleware';
 import express from 'express';
 
 const courseRouter = express.Router();
+
+courseRouter.put(
+  '/softDelete/:id',
+  authenticateJWT,
+  checkPermission([PERMISSIONS.MANAGE_COURSE]),
+  courseController.softDelete.bind(courseController)
+);
 
 export default courseRouter;
