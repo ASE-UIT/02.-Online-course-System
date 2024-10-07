026655eb915a7aa29c811e155e23a680ffe58c68
vudinhan2525 <vudinhan000@gmail.com>
2024-10-07 14:33:23 +0700

    upd design system & fix home page + components

--- fe-web/src/pages/HomePage/Home.jsx
+++ fe-web/src/pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import CategoryCard from "@/components/Category/CategoryCard";
 import { CourseCard } from "@/components/Courses/CourseCard";
-import { FaArrowRight } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 const Home = () => {
   return (
@@ -15,26 +16,26 @@ const Home = () => {
           ></div>
         </div>
         <div className="basis-[60%] justify-center gap-8 flex flex-col">
-          <header className="text-3xl font-bold text-white">
+          <header className="text-3xl text-white text-display/md/bold">
             Khám Phá Thế Giới Kiến Thức EduHub – Học Tập Mọi Lúc, Mọi Nơi
           </header>
-          <p className=" text-white">
+          <p className=" text-white text-text/md/regular">
             Với EduHub, tri thức nằm trong tầm tay bạn dù bạn ở bất kỳ đâu. Bạn
             có thể học tập mọi lúc, trên mọi thiết bị, tiếp cận khoá học từ các
             chuyên gia đầu ngành. Không còn giới hạn về thời gian hay không
             gian, EduHub mang lại một phương pháp học tập linh hoạt và hiện đại,
             giúp bạn chủ động xây dựng hành trình học tập của riêng mình.
           </p>
-          <div className="text-primary-color items-center gap-2 flex cursor-pointer transition-all hover:bg-gray-200 text-sm bg-white px-4 py-2 w-fit font-bold rounded-md ">
-            <p>Tham gia ngay</p>
-            <FaArrowRight />
+          <div className="text-primary-500 items-center gap-3 flex cursor-pointer transition-all hover:bg-gray-200 text-sm bg-white px-4 py-2 w-fit rounded-[4px] ">
+            <p className="text-text/md/semibold">Tham gia ngay</p>
+            <ArrowRight className="w-[24px] h-[24px]" />
           </div>
         </div>
       </div>
       <div className="px-24">
         <div className="flex justify-between mt-6">
-          <p className="text-2xl font-extrabold">Lịch học trực tiếp</p>
-          <p className="text-sm font-bold text-primary-color cursor-pointer">
+          <p className=" text-text/lg/bold">Lịch học trực tiếp</p>
+          <p className="text-text/md/semibold text-primary-500 cursor-pointer">
             Xem tất cả
           </p>
         </div>
@@ -49,15 +50,15 @@ const Home = () => {
                 author={"Tác giả"}
                 price={500000}
                 img={
-                  "https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CSSJdDE1TgT~XEce7zEFxHaiBYkErU7VYaC1AjxyFVGnIpNrGn8Q0oTUlD--uzUlYYJ9Ap7t~fqn-m4yfmF8b-j25anq0~THK4h~13BR1jGfel~WCFJHRwa0HqPlKzSocAlqbRxDvNh9Z8bCzFIF~~ifU95VFrp72fCAy7ekalCXk1FkWb2VSe0q5xENvT7dTY4VojE17qpRfJKihjquHmPlC3pJKmHXhUwkWD-2ZxnF3L8Vd-8IGUC1f61t1kGlOouGGiBlK8snPvgL7xmFzL-VLg69gJbCnw0uHPssIPSZYf99thi1EJ1N87ySGOvQIiw6ajiOQ9TM7TKunixi8w__"
+                  "https://s3-alpha-sig.figma.com/img/d628/d693/04cfd323cc9721e8f01abd84ea0386b4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N4Q9mPBB3zqehHB-~QJqRbcNQBXi7IgEH3DC1PFKN8PkzO4mfIvGhy1cVcr060cbBw9bSpE9Q1Bc8HFdmyrOFNOJAMBmIRkmbTevy027HMnCHdEQX~T8nw66Wd90ZqKMVoEjkQPimYXWxTx8xjlX5zn-~GbLOUeAV5h68LYKx7a49THmrEw7uY0aPT5qkm9YIlYWbCnSC59INHZATCuOZ~oaIfHAr7js23vELKUW0NDwEQyl7PYL3BhWlFD8VBSVuBD7hXjgVFhufjZ0pjd3ZmXbhilfW~Dez8CRuACsQSA85rXN24ZklS3oaPWtBMs4rGH3YM7pWVlAKB3TF0sJAg__"
                 }
               ></CourseCard>
             );
           })}
         </div>
         <div className="flex justify-between mt-6">
-          <p className="text-2xl font-extrabold">Top bán chạy</p>
-          <p className="text-sm font-bold text-primary-color cursor-pointer">
+          <p className="text-text/lg/bold">Top bán chạy</p>
+          <p className="text-text/md/semibold text-primary-500 cursor-pointer">
             Xem tất cả
           </p>
         </div>
@@ -72,15 +73,15 @@ const Home = () => {
                 author={"Tác giả"}
                 price={500000}
                 img={
-                  "https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CSSJdDE1TgT~XEce7zEFxHaiBYkErU7VYaC1AjxyFVGnIpNrGn8Q0oTUlD--uzUlYYJ9Ap7t~fqn-m4yfmF8b-j25anq0~THK4h~13BR1jGfel~WCFJHRwa0HqPlKzSocAlqbRxDvNh9Z8bCzFIF~~ifU95VFrp72fCAy7ekalCXk1FkWb2VSe0q5xENvT7dTY4VojE17qpRfJKihjquHmPlC3pJKmHXhUwkWD-2ZxnF3L8Vd-8IGUC1f61t1kGlOouGGiBlK8snPvgL7xmFzL-VLg69gJbCnw0uHPssIPSZYf99thi1EJ1N87ySGOvQIiw6ajiOQ9TM7TKunixi8w__"
+                  "https://s3-alpha-sig.figma.com/img/d628/d693/04cfd323cc9721e8f01abd84ea0386b4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N4Q9mPBB3zqehHB-~QJqRbcNQBXi7IgEH3DC1PFKN8PkzO4mfIvGhy1cVcr060cbBw9bSpE9Q1Bc8HFdmyrOFNOJAMBmIRkmbTevy027HMnCHdEQX~T8nw66Wd90ZqKMVoEjkQPimYXWxTx8xjlX5zn-~GbLOUeAV5h68LYKx7a49THmrEw7uY0aPT5qkm9YIlYWbCnSC59INHZATCuOZ~oaIfHAr7js23vELKUW0NDwEQyl7PYL3BhWlFD8VBSVuBD7hXjgVFhufjZ0pjd3ZmXbhilfW~Dez8CRuACsQSA85rXN24ZklS3oaPWtBMs4rGH3YM7pWVlAKB3TF0sJAg__"
                 }
               ></CourseCard>
             );
           })}
         </div>
         <div className="flex justify-between mt-6">
-          <p className="text-2xl font-extrabold">Khóa học mới ra mắt</p>
-          <p className="text-sm font-bold text-primary-color cursor-pointer">
+          <p className="text-text/lg/bold">Khóa học mới ra mắt</p>
+          <p className="text-text/md/semibold text-primary-500 cursor-pointer">
             Xem tất cả
           </p>
         </div>
@@ -95,15 +96,15 @@ const Home = () => {
                 author={"Tác giả"}
                 price={500000}
                 img={
-                  "https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CSSJdDE1TgT~XEce7zEFxHaiBYkErU7VYaC1AjxyFVGnIpNrGn8Q0oTUlD--uzUlYYJ9Ap7t~fqn-m4yfmF8b-j25anq0~THK4h~13BR1jGfel~WCFJHRwa0HqPlKzSocAlqbRxDvNh9Z8bCzFIF~~ifU95VFrp72fCAy7ekalCXk1FkWb2VSe0q5xENvT7dTY4VojE17qpRfJKihjquHmPlC3pJKmHXhUwkWD-2ZxnF3L8Vd-8IGUC1f61t1kGlOouGGiBlK8snPvgL7xmFzL-VLg69gJbCnw0uHPssIPSZYf99thi1EJ1N87ySGOvQIiw6ajiOQ9TM7TKunixi8w__"
+                  "https://s3-alpha-sig.figma.com/img/d628/d693/04cfd323cc9721e8f01abd84ea0386b4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N4Q9mPBB3zqehHB-~QJqRbcNQBXi7IgEH3DC1PFKN8PkzO4mfIvGhy1cVcr060cbBw9bSpE9Q1Bc8HFdmyrOFNOJAMBmIRkmbTevy027HMnCHdEQX~T8nw66Wd90ZqKMVoEjkQPimYXWxTx8xjlX5zn-~GbLOUeAV5h68LYKx7a49THmrEw7uY0aPT5qkm9YIlYWbCnSC59INHZATCuOZ~oaIfHAr7js23vELKUW0NDwEQyl7PYL3BhWlFD8VBSVuBD7hXjgVFhufjZ0pjd3ZmXbhilfW~Dez8CRuACsQSA85rXN24ZklS3oaPWtBMs4rGH3YM7pWVlAKB3TF0sJAg__"
                 }
               ></CourseCard>
             );
           })}
         </div>
         <div className="flex justify-between mt-6">
-          <p className="text-2xl font-extrabold">
-            Khám phá <span className="text-primary-color">EduHub</span>
+          <p className="text-text/lg/bold">
+            Khám phá <span className="text-primary-500">EduHub</span>
           </p>
         </div>
         <div className="grid grid-cols-3 mt-4  gap-[1rem]">
@@ -111,32 +112,32 @@ const Home = () => {
             return (
               <CategoryCard
                 category={"Tiếng Anh"}
-                subcategory={[
-                  "Grammar cho người mới",
-                  "Speaking",
-                  "Writing",
-                  "Listening",
-                ]}
+                subcategory={["Speaking"]}
                 img={
-                  "https://s3-alpha-sig.figma.com/img/7f12/ea13/00756f144a0fb5daaf68dbfc01103a46?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CSSJdDE1TgT~XEce7zEFxHaiBYkErU7VYaC1AjxyFVGnIpNrGn8Q0oTUlD--uzUlYYJ9Ap7t~fqn-m4yfmF8b-j25anq0~THK4h~13BR1jGfel~WCFJHRwa0HqPlKzSocAlqbRxDvNh9Z8bCzFIF~~ifU95VFrp72fCAy7ekalCXk1FkWb2VSe0q5xENvT7dTY4VojE17qpRfJKihjquHmPlC3pJKmHXhUwkWD-2ZxnF3L8Vd-8IGUC1f61t1kGlOouGGiBlK8snPvgL7xmFzL-VLg69gJbCnw0uHPssIPSZYf99thi1EJ1N87ySGOvQIiw6ajiOQ9TM7TKunixi8w__"
+                  "https://s3-alpha-sig.figma.com/img/d628/d693/04cfd323cc9721e8f01abd84ea0386b4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N4Q9mPBB3zqehHB-~QJqRbcNQBXi7IgEH3DC1PFKN8PkzO4mfIvGhy1cVcr060cbBw9bSpE9Q1Bc8HFdmyrOFNOJAMBmIRkmbTevy027HMnCHdEQX~T8nw66Wd90ZqKMVoEjkQPimYXWxTx8xjlX5zn-~GbLOUeAV5h68LYKx7a49THmrEw7uY0aPT5qkm9YIlYWbCnSC59INHZATCuOZ~oaIfHAr7js23vELKUW0NDwEQyl7PYL3BhWlFD8VBSVuBD7hXjgVFhufjZ0pjd3ZmXbhilfW~Dez8CRuACsQSA85rXN24ZklS3oaPWtBMs4rGH3YM7pWVlAKB3TF0sJAg__"
                 }
                 key={idx}
               ></CategoryCard>
             );
           })}
         </div>
+        <div className="mt-4 flex justify-center">
+          <Button className="text-text/md/semibold text-white">
+            Xem tất cả
+          </Button>
+        </div>
       </div>
       <div className="w-full mt-8 px-[15%] h-[355px] bg-[#EEFEE7] flex">
         <div className="basis-[60%] flex flex-col gap-6 justify-center">
-          <p className="text-3xl font-extrabold text-[#1C5413]">
+          <p className="text-display/md/bold text-success-900">
             Trở thành giảng viên của EduHub
           </p>
-          <p>
+          <p className="text-text/md/regular">
             Giảng viên trên toàn thế giới đã và đang dạy cho hàng triệu học viên
             trên EduHub. EduHub cung cấp công cụ và kỹ năng để giúp bạn hoàn
             thiện quá trình giảng dạy tốt hơn.
           </p>
-          <div className="bg-[#1F7E0D] px-4 w-fit text-white text-sm font-bold rounded-md cursor-pointer hover:opacity-90 transition-all py-2">
+          <div className="bg-[#1F7E0D] px-4 w-fit text-white text-text/md/semibold rounded-md cursor-pointer hover:opacity-90 transition-all py-2">
             Bắt đầu giảng dạy
           </div>
         </div>
