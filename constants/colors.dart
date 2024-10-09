a7ad5a8e359c9f7fdff64828d904303116b06838
nhdhieuu <22520439@gm.uit.edu.vn>
2024-10-10 00:57:13 +0700

    feat(loginscreen): update project structure

---
+++ constants/colors.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primary50 = Color(0xFFE4F2FF);
+  static const Color primary100 = Color(0xFFCEE5FF);
+  static const Color primary200 = Color(0xFFA7D1FF);
+  static const Color primary300 = Color(0xFF72B6FF);
+  static const Color primary400 = Color(0xFF3C9BFF);
+  static const Color primary500 = Color(0xFF0085FF);
+  static const Color primary600 = Color(0xFF0061D1);
+  static const Color primary700 = Color(0xFF0047A7);
+  static const Color primary800 = Color(0xFF003380);
+  static const Color primary900 = Color(0xFF001F5E);
+  static const Color primary950 = Color(0xFF00115E);
+
+  // Warning Colors
+  static const Color warning50 = Color(0xFFFFF9DF);
+  static const Color warning100 = Color(0xFFFFF5BE);
+  static const Color warning200 = Color(0xFFFFEA6C);
+  static const Color warning300 = Color(0xFFFFD55A);
+  static const Color warning400 = Color(0xFFFFC535);
+  static const Color warning500 = Color(0xFFFFAD00);
+  static const Color warning600 = Color(0xFFFF9D02);
+  static const Color warning700 = Color(0xFFE17702);
+  static const Color warning800 = Color(0xFFB75D0A);
+  static const Color warning900 = Color(0xFF954B0C);
+  static const Color warning950 = Color(0xFF562800);
+
+  // Error Colors
+  static const Color error50 = Color(0xFFFFF3F3);
+  static const Color error100 = Color(0xFFFFE4E4);
+  static const Color error200 = Color(0xFFFCCCCC);
+  static const Color error300 = Color(0xFFFFAAAA);
+  static const Color error400 = Color(0xFFFF8686);
+  static const Color error500 = Color(0xFFFF5959);
+  static const Color error600 = Color(0xFFFF0E0E);
+  static const Color error700 = Color(0xFFE00606);
+  static const Color error800 = Color(0xFFA61515);
+  static const Color error900 = Color(0xFF8A1515);
+  static const Color error950 = Color(0xFF540505);
+
+  // Success Colors
+  static const Color success50 = Color(0xFFEEFEE7);
+  static const Color success100 = Color(0xFFD9FBCB);
+  static const Color success200 = Color(0xFFB5F5A1);
+  static const Color success300 = Color(0xFF89EB72);
+  static const Color success400 = Color(0xFF66DB4B);
+  static const Color success500 = Color(0xFF3BD313);
+  static const Color success600 = Color(0xFF2EB40D);
+  static const Color success700 = Color(0xFF23810F);
+  static const Color success800 = Color(0xFF1C5413);
+  static const Color success900 = Color(0xFF022F04);
+  static const Color success950 = Color(0xFF002F04);
+
+  // Gray Colors
+  static const Color gray50 = Color(0xFFF3F3F3);
+  static const Color gray100 = Color(0xFFE6E6E6);
+  static const Color gray200 = Color(0xFFCCCCCC);
+  static const Color gray300 = Color(0xFFB3B3B3);
+  static const Color gray400 = Color(0xFF999999);
+  static const Color gray500 = Color(0xFF808080);
+  static const Color gray600 = Color(0xFF666666);
+  static const Color gray700 = Color(0xFF4D4D4D);
+  static const Color gray800 = Color(0xFF333333);
+  static const Color gray900 = Color(0xFF1A1A1A);
+  static const Color gray950 = Color(0xFF0D0D0D);
+
+  // Black and White
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
