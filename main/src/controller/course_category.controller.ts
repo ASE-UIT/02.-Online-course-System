36bfd4b74a0c8dc6648d1ed5e375acd01f15e96b
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2024-10-24 23:07:15 +0700

    feat(coursecategory): find all categories

--- main/src/controller/course_category.controller.ts
+++ main/src/controller/course_category.controller.ts
@@ -1,7 +1,10 @@
 import { IBaseCrudController } from '@/controller/interfaces/i.base-curd.controller';
+import { CourseCategoryRes } from '@/dto/course_category/course_category.res';
 import { CourseCategory } from '@/models/course_category.model';
 import { ICourseCategoryService } from '@/service/interface/i.course_category.service';
 import { ITYPES } from '@/types/interface.types';
+import { convertToDto } from '@/utils/dto-convert/convert-to-dto.util';
+import { NextFunction, Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
 
 @injectable()
@@ -14,5 +17,14 @@ export class CourseCategoryController {
   ) {
     this.courseCategoryService = courseCategoryService;
     this.common = common;
+  }
+  async findAll(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const categories = await this.courseCategoryService.findAll();
+      const resultDto = convertToDto(CourseCategoryRes, categories);
+      res.send_ok('Get all categories successfully', resultDto);
+    } catch (error) {
+      next(error);
+    }
   }
 }
