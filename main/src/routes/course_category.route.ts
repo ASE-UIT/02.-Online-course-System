870c1117fc3189789f99e342403bd72268769af8
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2024-10-25 21:39:29 +0700

    feat(coursecategory): find all category

--- main/src/routes/course_category.route.ts
+++ main/src/routes/course_category.route.ts
@@ -1,5 +1,8 @@
+import { courseCategoryController } from '@/container/course_category.container';
+import { authenticateJWT } from '@/middleware/authenticate.middleware';
 import express from 'express';
 
 const courseCategoryRouter = express.Router();
 
+courseCategoryRouter.get('/', courseCategoryController.findAll.bind(courseCategoryController));
 export default courseCategoryRouter;
