b40cee36fadf075c12c0d93007b4e15fb2702433
vudinhan2525 <vudinhan000@gmail.com>
2024-11-02 13:12:21 +0700

    map api course

--- fe-web/src/pages/CourseDetailPage/RelatedCourse.jsx
+++ fe-web/src/pages/CourseDetailPage/RelatedCourse.jsx
@@ -1,11 +1,16 @@
 import { CourseCard } from "@/components/Courses/CourseCard";
-import { CourseData } from "@/components/Courses/Course";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+export const CourseData = [
+  {
+    img: "https://s3-alpha-sig.figma.com/img/d628/d693/04cfd323cc9721e8f01abd84ea0386b4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N4Q9mPBB3zqehHB-~QJqRbcNQBXi7IgEH3DC1PFKN8PkzO4mfIvGhy1cVcr060cbBw9bSpE9Q1Bc8HFdmyrOFNOJAMBmIRkmbTevy027HMnCHdEQX~T8nw66Wd90ZqKMVoEjkQPimYXWxTx8xjlX5zn-~GbLOUeAV5h68LYKx7a49THmrEw7uY0aPT5qkm9YIlYWbCnSC59INHZATCuOZ~oaIfHAr7js23vELKUW0NDwEQyl7PYL3BhWlFD8VBSVuBD7hXjgVFhufjZ0pjd3ZmXbhilfW~Dez8CRuACsQSA85rXN24ZklS3oaPWtBMs4rGH3YM7pWVlAKB3TF0sJAg__",
+    name: "Tiêu đề",
+    author: "Tác giả",
+    rating: "4",
+    ratingNum: "n",
+    price: 100000,
+    promotion: "Best seller",
+  },
+];
 function RelatedCourse() {
   return (
     <div className="related flex gap-[20px] flex-col">
@@ -15,7 +20,7 @@ function RelatedCourse() {
           <CarouselContent>
             {CourseData.map((item, index) => (
               <CarouselItem className="basis-1/3" key={index}>
-                <CourseCard key={index} {...item} />
+                <CourseCard key={index} course={item} />
               </CarouselItem>
             ))}
           </CarouselContent>
