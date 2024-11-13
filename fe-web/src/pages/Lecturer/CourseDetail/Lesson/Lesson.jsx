525e331c326ef52c2ee4dc62f87790b59aa481ee
vudinhan2525 <vudinhan000@gmail.com>
2024-11-13 19:40:58 +0700

    upd

--- fe-web/src/pages/Lecturer/CourseDetail/Lesson/Lesson.jsx
+++ fe-web/src/pages/Lecturer/CourseDetail/Lesson/Lesson.jsx
@@ -1,4 +1,6 @@
 import { FileText, Pen, PlusIcon, Trash2 } from "lucide-react";
+import { useState } from "react";
+import AddLessonForm from "./AddLessonForm";
 const lessons = [
   {
     name: "Bài 1: Giới thiệu",
@@ -47,64 +49,79 @@ const LESSONS_TABLE_HEADER = [
   },
 ];
 export default function Lesson() {
+  const [showAddLessonForm, setShowAddLessonForm] = useState(false);
   return (
     <div className="p-[20px]">
-      <div className="flex items-center justify-between">
+      {!showAddLessonForm && (
         <div>
-          <p className="text-display/md/medium">DANH SÁCH BÀI HỌC</p>
-          <p className="text-text/md/medium">(Giữ và kéo thả để sắp xếp vị trí phần học và bài họ)</p>
-        </div>
-        <div className="flex items-center py-[10px] px-[16px] bg-primary-500 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-primary-600 transition-all">
-          <PlusIcon />
-          <p className="text-text/md/medium">Phần học mới</p>
-        </div>
-      </div>
-      <div className="w-full p-[10px] border-[1px] border-gray-500 rounded-[4px] mt-4">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b-[1px]">
-              {LESSONS_TABLE_HEADER.map((header) => (
-                <th key={header.label} className={` text-start py-2  text-base-black`} style={{ width: header.width }}>
-                  <span className="text-text/md/regular">{header.label}</span>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {lessons.map((lesson, index) => {
-              return (
-                <tr key={`row-${index}`} className={`border-b-[1px] border-dashed	`}>
-                  <td className={`text-start text-text/md/regular items-center  border-gray-300 py-3 flex gap-2`}>
-                    <FileText className="w-[16px] h-[16px]" />
-                    <p>{lesson.name}</p>
-                  </td>
-                  <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
-                  <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
-                  <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
-                  <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
-                  <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>
-                    <div className="flex gap-2">
-                      <Pen className="w-[16px] cursor-pointer h-[16px] text-primary-400" />
-                      <Trash2 className="w-[16px] cursor-pointer h-[16px] text-error-500" />
-                    </div>
-                  </td>
+          <div className="flex items-center justify-between">
+            <div>
+              <p className="text-display/md/medium">DANH SÁCH BÀI HỌC</p>
+              <p className="text-text/md/medium">(Giữ và kéo thả để sắp xếp vị trí phần học và bài họ)</p>
+            </div>
+            <div className="flex items-center py-[10px] px-[16px] bg-primary-500 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-primary-600 transition-all">
+              <PlusIcon />
+              <p className="text-text/md/medium">Phần học mới</p>
+            </div>
+          </div>
+          <div className="w-full p-[10px] border-[1px] border-gray-500 rounded-[4px] mt-4">
+            <table className="w-full">
+              <thead>
+                <tr className="border-b-[1px]">
+                  {LESSONS_TABLE_HEADER.map((header) => (
+                    <th
+                      key={header.label}
+                      className={` text-start py-2  text-base-black`}
+                      style={{ width: header.width }}
+                    >
+                      <span className="text-text/md/regular">{header.label}</span>
+                    </th>
+                  ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div className="flex gap-3 mt-2 items-center">
-          <div className="flex items-center py-[10px] px-[16px] bg-success-600 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-success-700 transition-all">
-            <PlusIcon />
-            <p className="text-text/md/medium">Bài học mới</p>
+              </thead>
+              <tbody>
+                {lessons.map((lesson, index) => {
+                  return (
+                    <tr key={`row-${index}`} className={`border-b-[1px] border-dashed	`}>
+                      <td className={`text-start text-text/md/regular items-center  border-gray-300 py-3 flex gap-2`}>
+                        <FileText className="w-[16px] h-[16px]" />
+                        <p>{lesson.name}</p>
+                      </td>
+                      <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
+                      <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
+                      <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
+                      <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>{}</td>
+                      <td className={`text-start text-text/md/regular  border-gray-300 py-3`}>
+                        <div className="flex gap-2">
+                          <Pen className="w-[16px] cursor-pointer h-[16px] text-primary-400" />
+                          <Trash2 className="w-[16px] cursor-pointer h-[16px] text-error-500" />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <div className="flex gap-3 mt-2 items-center">
+              <div
+                onClick={() => {
+                  setShowAddLessonForm(true);
+                }}
+                className="flex items-center py-[10px] px-[16px] bg-success-600 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-success-700 transition-all"
+              >
+                <PlusIcon />
+                <p className="text-text/md/medium">Bài học mới</p>
+              </div>
+              <div className="flex items-center py-[10px] px-[16px] bg-warning-700 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-warning-800 transition-all">
+                <PlusIcon />
+                <p className="text-text/md/medium">Bài trắc nghiệm</p>
+              </div>
+              <p className="text-text/md/regular">Một phần học nên có một bài trắc nghiệm</p>
+            </div>
           </div>
-          <div className="flex items-center py-[10px] px-[16px] bg-warning-700 rounded-[8px] gap-2 text-white cursor-pointer hover:bg-warning-800 transition-all">
-            <PlusIcon />
-            <p className="text-text/md/medium">Bài trắc nghiệm</p>
-          </div>
-          <p className="text-text/md/regular">Một phần học nên có một bài trắc nghiệm</p>
         </div>
-      </div>
+      )}
+      {showAddLessonForm && <AddLessonForm setShowAddLessonForm={setShowAddLessonForm} />}
     </div>
   );
 }
