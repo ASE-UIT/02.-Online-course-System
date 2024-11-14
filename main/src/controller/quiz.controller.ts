a34750278bb6b68317b0cd7a4525b232abb26d68
Hung Mai <126455648+mhhung0811@users.noreply.github.com>
2024-11-15 01:32:11 +0700

    feat(quiz api): is done quiz api

--- main/src/controller/quiz.controller.ts
+++ main/src/controller/quiz.controller.ts
@@ -4,16 +4,21 @@ import { IQuizService } from '@/service/interface/i.quiz.service';
 import { ITYPES } from '@/types/interface.types';
 import { NextFunction, Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
+import { StudentCompleteQuiz } from '@/models/student_complete_quiz.model';
+import { IStudentCompleteQuizRepository } from '@/repository/interface/i.student_complete_quiz.repository';
 
 @injectable()
 export class QuizController {
   public common: IBaseCrudController<Quiz>;
   private quizService: IQuizService<Quiz>;
+  private studentCompleteQuizRepository: IStudentCompleteQuizRepository<StudentCompleteQuiz>;
   constructor(
     @inject('QuizService') quizService: IQuizService<Quiz>,
+    @inject('StudentCompleteQuizRepository') studentCompleteQuizRepository: IStudentCompleteQuizRepository<StudentCompleteQuiz>,
     @inject(ITYPES.Controller) common: IBaseCrudController<Quiz>
   ) {
     this.quizService = quizService;
+    this.studentCompleteQuizRepository = studentCompleteQuizRepository;
     this.common = common;
   }
 
@@ -55,6 +60,24 @@ export class QuizController {
         res.send_ok('Right answer', result);
       } else {
         res.send_ok('Wrong answer', result);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  /**
+   * * GET /quiz/done?studentId&quizId
+   */
+  async doneQuiz(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const studentId = req.query.studentId?.toString();
+      const quizId = req.query.quizId?.toString();
+      const result = await this.studentCompleteQuizRepository.findOne({ filter: { studentId, quizId } });
+      if (result) {
+        res.send_ok('Quiz done', result);
+      } else {
+        res.send_ok('Quiz not done', result);
       }
     } catch (error) {
       next(error);
--- main/src/routes/quiz.route.ts
+++ main/src/routes/quiz.route.ts
@@ -4,6 +4,7 @@ const quizRouter = express.Router();
 
 quizRouter
   .get('/:lessonId', quizController.findByLessonId.bind(quizController))
-  .get('/answer', quizController.answerQuiz.bind(quizController));
+  .get('/answer', quizController.answerQuiz.bind(quizController))
+  .get('/done', quizController.doneQuiz.bind(quizController));
 
 export default quizRouter;
