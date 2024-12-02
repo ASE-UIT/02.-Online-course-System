81706079606dca0badd1442e77434700eaa7f359
vudinhan2525 <vudinhan000@gmail.com>
2024-12-02 23:00:09 +0700

    update ui learning course

--- fe-web/src/router/Router.jsx
+++ fe-web/src/router/Router.jsx
@@ -1,75 +1,34 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Navigate
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
 import withSuspense from "./WithSuspense";
 
 const UserLayout = withSuspense(lazy(() => import("../layouts/UserLayout")));
 const Home = withSuspense(lazy(() => import("../pages/HomePage/Home")));
-const CourseDetail = withSuspense(
-  lazy(() => import("@/pages/CourseDetailPage/CourseDetail"))
-);
+const CourseDetail = withSuspense(lazy(() => import("@/pages/CourseDetailPage/CourseDetail")));
 const SignUp = withSuspense(lazy(() => import("@/pages/SignUp/SignUp")));
 const SignIn = withSuspense(lazy(() => import("@/pages/SignIn/SignIn")));
 const StepOne = withSuspense(lazy(() => import("@/pages/SignUp/StepOne")));
-const VerifyCode = withSuspense(
-  lazy(() => import("@/pages/SignUp/VerifyCode"))
-);
-const ResultPage = withSuspense(
-  lazy(() => import("@/pages/Result/ResultPage"))
-);
+const VerifyCode = withSuspense(lazy(() => import("@/pages/SignUp/VerifyCode")));
+const ResultPage = withSuspense(lazy(() => import("@/pages/Result/ResultPage")));
 const InfoInput = withSuspense(lazy(() => import("@/pages/SignUp/InfoInput")));
-const LecturerLayout = withSuspense(
-  lazy(() => import("@/layouts/LecturerLayout"))
-);
-const LecturerSignUp = withSuspense(
-  lazy(() => import("@/pages/Lecturer/SignUp/LecturerSignUp"))
-);
-const LecturerResultPage = withSuspense(
-  lazy(() => import("@/pages/Lecturer/Result/ResultPage"))
-);
-const LecturerLandingPage = withSuspense(
-  lazy(() => import("@/pages/Lecturer/LandingPage/LecturerLandingPage.jsx"))
-);
-const TeacherCoursePage = withSuspense(
-  lazy(() => import("@/pages/Lecturer/TeacherCoursePage/TeacherCoursePage"))
-);
-const LecturerCourseDetail = withSuspense(
-  lazy(() => import("@/pages/Lecturer/CourseDetail/LecturerCourseDetail"))
-);
-const CourseAdd = withSuspense(
-  lazy(() => import("@/pages/Lecturer/TeacherCoursePage/AddCourse/CourseAdd"))
-);
-const SignInForm = withSuspense(
-  lazy(() => import("@/pages/SignIn/SignInForm"))
-);
-const ForgotPassword = withSuspense(
-  lazy(() => import("@/pages/SignIn/ForgotPassword"))
-);
-const ResetPassword = withSuspense(
-  lazy(() => import("@/pages/SignIn/ResetPassword"))
-);
+const LecturerLayout = withSuspense(lazy(() => import("@/layouts/LecturerLayout")));
+const LecturerSignUp = withSuspense(lazy(() => import("@/pages/Lecturer/SignUp/LecturerSignUp")));
+const LecturerResultPage = withSuspense(lazy(() => import("@/pages/Lecturer/Result/ResultPage")));
+const LecturerLandingPage = withSuspense(lazy(() => import("@/pages/Lecturer/LandingPage/LecturerLandingPage.jsx")));
+const TeacherCoursePage = withSuspense(lazy(() => import("@/pages/Lecturer/TeacherCoursePage/TeacherCoursePage")));
+const LecturerCourseDetail = withSuspense(lazy(() => import("@/pages/Lecturer/CourseDetail/LecturerCourseDetail")));
+const CourseAdd = withSuspense(lazy(() => import("@/pages/Lecturer/TeacherCoursePage/AddCourse/CourseAdd")));
+const SignInForm = withSuspense(lazy(() => import("@/pages/SignIn/SignInForm")));
+const ForgotPassword = withSuspense(lazy(() => import("@/pages/SignIn/ForgotPassword")));
+const ResetPassword = withSuspense(lazy(() => import("@/pages/SignIn/ResetPassword")));
 const NotFound = withSuspense(lazy(() => import("@/pages/NotFound")));
-const SearchPage = withSuspense(
-  lazy(() => import("@/pages/searchPage/searchPage"))
-);
-const DefaultLayout = withSuspense(
-  lazy(() => import("@/layouts/DefaultLayout"))
-);
-const LecturerSignIn = withSuspense(
-  lazy(() => import("@/pages/Lecturer/SignIn/LecturerSignIn"))
-);
-const CartPage = withSuspense(
-  lazy(() => import("@/pages/CartPage/CartPage.jsx"))
-);
-const CourseList = withSuspense(
-  lazy(() => import("@/pages/CourseList/CourseList.jsx"))
-);
-
+const SearchPage = withSuspense(lazy(() => import("@/pages/searchPage/searchPage")));
+const DefaultLayout = withSuspense(lazy(() => import("@/layouts/DefaultLayout")));
+const LecturerSignIn = withSuspense(lazy(() => import("@/pages/Lecturer/SignIn/LecturerSignIn")));
+const CartPage = withSuspense(lazy(() => import("@/pages/CartPage/CartPage.jsx")));
+const CourseList = withSuspense(lazy(() => import("@/pages/CourseList/CourseList.jsx")));
+const LearningPage = withSuspense(lazy(() => import("@/pages/LearningPage/LearningPage.jsx")));
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
@@ -78,28 +37,16 @@ const router = createBrowserRouter(
           <Route index element={<Home />} />
           <Route path="course/:id" element={<CourseDetail />} />
           <Route path="sign-up" element={<SignUp />}>
-            <Route
-              path="*"
-              element={<Navigate to={"/web/sign-up/step1/email"} />}
-            />
+            <Route path="*" element={<Navigate to={"/web/sign-up/step1/email"} />} />
             <Route path="step1/:signUpType" element={<StepOne />} />
-            <Route
-              path="step2/:signUpType/:emailOrPhone"
-              element={<VerifyCode />}
-            />
+            <Route path="step2/:signUpType/:emailOrPhone" element={<VerifyCode />} />
           </Route>
           <Route path="sign-in" element={<SignIn />}>
             <Route path="*" element={<Navigate to={"/web/sign-in"} />} />
             <Route index element={<SignInForm />} />
             <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route
-              path="step2/:signUpType/:emailOrPhone"
-              element={<VerifyCode />}
-            />
-            <Route
-              path="reset-password/:emailOrPhone/:otp"
-              element={<ResetPassword />}
-            />
+            <Route path="step2/:signUpType/:emailOrPhone" element={<VerifyCode />} />
+            <Route path="reset-password/:emailOrPhone/:otp" element={<ResetPassword />} />
           </Route>
           <Route path="search/" element={<SearchPage />} />
           <Route path="callback" element={<InfoInput />} />
@@ -118,6 +65,7 @@ const router = createBrowserRouter(
           </Route>
         </Route>
       </Route>
+      <Route path="/web/learning/:courseId" element={<LearningPage />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   ),
@@ -127,8 +75,8 @@ const router = createBrowserRouter(
       v7_fetcherPersist: true,
       v7_normalizeFormMethod: true,
       v7_partialHydration: true,
-      v7_skipActionStatusRevalidation: true
-    }
+      v7_skipActionStatusRevalidation: true,
+    },
   }
 );
 
---
+++ fe-web/src/store/slices/learningSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  moduleSlt: -1,
+  lessonSlt: -1,
+  lesson: null,
+};
+const learningSlice = createSlice({
+  name: "learning",
+  initialState,
+  reducers: {
+    setLearning: (state, action) => {
+      const newState = { ...state, ...action.payload };
+      return newState;
+    },
+  },
+  //extraReducers: (builder) => {},
+});
+export const { setLearning } = learningSlice.actions;
+export default learningSlice.reducer;
