4d03238501ecc2b4ad846e31962a1c9dc0d327fb
chabuuuu <haphuthinh332004@gmail.com>
2024-12-06 13:48:48 +0700

    feat(lecturer stat): add get lecturer stats logic

--- main/src/service/interface/i.course.service.ts
+++ main/src/service/interface/i.course.service.ts
@@ -6,11 +6,12 @@ import { Course } from '@/models/course.model';
 import { CreateCourseRequest } from '@/dto/course/create-course.req';
 import { CourseSearchFilterReq } from '@/dto/course/course-search-filter.req';
 import { CourseSearchSortReq } from '@/dto/course/course-search-sort.req';
+import { CourseDetailRes } from '@/dto/course/course-detail.res';
 
 export interface ICourseService<T extends BaseModelType> extends IBaseCrudService<T> {
   lecturerCreateCourse(data: CreateCourseRequest, lecturerId: string): Promise<Course>;
   update(id: string, data: UpdateCourseRequest): Promise<Course>;
   getClosetLiveCourse(amount: number): Promise<Course[]>;
   search(filters: CourseSearchFilterReq[], sort: CourseSearchSortReq, rpp: number, page: number): Promise<Course[]>;
-  getCourseDetail(courseId: string): Promise<Course>;
+  getCourseDetail(courseId: string): Promise<CourseDetailRes>;
 }
