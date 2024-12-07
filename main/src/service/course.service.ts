356f3b407d673ce075ce0286064bdb27a02209fb
chabuuuu <haphuthinh332004@gmail.com>
2024-12-07 15:30:33 +0700

    feat(course): get course learning for get on learning screen

--- main/src/service/course.service.ts
+++ main/src/service/course.service.ts
@@ -25,22 +25,96 @@ import { CourseDetailRes } from '@/dto/course/course-detail.res';
 import redis from '@/utils/redis/redis.util';
 import { RedisSchemaEnum } from '@/enums/redis-schema.enum';
 import { TIME_CONSTANTS } from '@/constants/time.constants';
+import { CourseLearningRes } from '@/dto/course/course-learning.res';
+import { IStudentCompleteQuizRepository } from '@/repository/interface/i.student_complete_quiz.repository';
+import { StudentCompleteQuiz } from '@/models/student_complete_quiz.model';
+import { GetLearningProgressRes } from '@/dto/student_complete_lesson/get-learning-progress.res';
 
 @injectable()
 export class CourseService extends BaseCrudService<Course> implements ICourseService<Course> {
   private courseRepository: ICourseRepository<Course>;
   private courseCategoryRepository: ICourseCategoryRepository<CourseCategory>;
   private lecturerRepository: ILecturerRepository<Lecturer>;
+  private studentCompleteQuizRepository: IStudentCompleteQuizRepository<StudentCompleteQuiz>;
 
   constructor(
     @inject('CourseRepository') courseRepository: ICourseRepository<Course>,
     @inject('LecturerRepository') lecturerRepository: ILecturerRepository<Lecturer>,
+    @inject('StudentCompleteQuizRepository')
+    studentCompleteQuizRepository: IStudentCompleteQuizRepository<StudentCompleteQuiz>,
     @inject('CourseCategoryRepository') courseCategoryRepository: ICourseCategoryRepository<CourseCategory>
   ) {
     super(courseRepository);
     this.courseRepository = courseRepository;
     this.courseCategoryRepository = courseCategoryRepository;
     this.lecturerRepository = lecturerRepository;
+    this.studentCompleteQuizRepository = studentCompleteQuizRepository;
+  }
+
+  /**
+   * This method is diffrent from get course detail by id
+   * because => its return course detail with learning progress of each lesson and also
+   * quiz progress of each quiz
+   * @param courseId
+   */
+  async getCourseLearning(
+    courseId: string,
+    studentId: string,
+    learningProgress: GetLearningProgressRes
+  ): Promise<CourseLearningRes> {
+    let courseDetail: Course;
+    //Check in cache
+    const courseDetailCache = await redis.get(`${RedisSchemaEnum.courseDetail}:${courseId}`);
+    if (courseDetailCache) {
+      courseDetail = JSON.parse(courseDetailCache) as Course;
+    } else {
+      const courseDetailInDb = await this.courseRepository.getCourseDetail(courseId);
+
+      if (!courseDetailInDb) {
+        throw new BaseError('COURSE_NOT_FOUND', 'Không tìm thấy khóa học');
+      }
+
+      courseDetail = courseDetailInDb as Course;
+    }
+
+    let courseLearningRes = new CourseLearningRes();
+
+    courseLearningRes = courseDetail as unknown as CourseLearningRes;
+
+    for (const lessonPart of courseLearningRes.lessonParts) {
+      for (const lesson of lessonPart.lessons) {
+        //Check if lesson is complete from learning progress
+        const isCompleteLesson = learningProgress.lessonLearnProgresses.find((x) => x.lessonId === lesson.id);
+        if (isCompleteLesson) {
+          lesson.isComplete = isCompleteLesson.isComplete;
+          lesson.progress = isCompleteLesson.progress;
+          lesson.completeAt = isCompleteLesson.completeAt;
+        } else {
+          lesson.isComplete = false;
+          lesson.progress = 0;
+        }
+      }
+
+      for (const quiz of lessonPart.quizzes) {
+        const isCompleteQuiz = await this.studentCompleteQuizRepository.findOne({
+          filter: { studentId, quizId: quiz.id }
+        });
+
+        if (isCompleteQuiz) {
+          quiz.isComplete = true;
+        } else {
+          quiz.isComplete = false;
+        }
+      }
+    }
+
+    courseLearningRes.learningProgress = {
+      totalCompleteLesson: learningProgress.totalCompleteLesson,
+      totalLesson: learningProgress.totalLesson,
+      courseProgress: learningProgress.courseProgress
+    };
+
+    return courseLearningRes;
   }
 
   /**
@@ -52,7 +126,14 @@ export class CourseService extends BaseCrudService<Course> implements ICourseSer
     //Check in cache
     const courseDetailCache = await redis.get(`${RedisSchemaEnum.courseDetail}:${courseId}`);
     if (courseDetailCache) {
-      return JSON.parse(courseDetailCache) as CourseDetailRes;
+      const parsedCourseDetail = JSON.parse(courseDetailCache) as CourseDetailRes;
+      const lecturerStats: LecturerStatsDto = await this.lecturerRepository.getLecturerStats(
+        parsedCourseDetail.lecturerId
+      );
+
+      parsedCourseDetail.lecturerStats = lecturerStats;
+
+      return parsedCourseDetail;
     }
 
     const course = await this.courseRepository.getCourseDetail(courseId);
@@ -60,6 +141,14 @@ export class CourseService extends BaseCrudService<Course> implements ICourseSer
     if (!course) {
       throw new BaseError('COURSE_NOT_FOUND', 'Không tìm thấy khóa học');
     }
+
+    //Caching course detail
+    redis.set(
+      `${RedisSchemaEnum.courseDetail}:${courseId}`,
+      JSON.stringify(course),
+      'EX',
+      (TIME_CONSTANTS.MINUTE / 1000) * 30 //30 minutes
+    );
 
     //Calculate lecturer stat: average rating, total rating, total student, total course
     const lecturerStats: LecturerStatsDto = await this.lecturerRepository.getLecturerStats(course.lecturerId);
@@ -77,14 +166,6 @@ export class CourseService extends BaseCrudService<Course> implements ICourseSer
     //     }
     //   }
     // }
-
-    //Caching course detail
-    redis.set(
-      `${RedisSchemaEnum.courseDetail}:${courseId}`,
-      JSON.stringify(course),
-      'EX',
-      (TIME_CONSTANTS.MINUTE / 1000) * 30 //30 minutes
-    );
 
     return course as CourseDetailRes;
   }
