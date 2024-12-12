fcbb0c00fe449b0c778575ff4119da6997bc8a24
nhdhieuu <22520439@gm.uit.edu.vn>
2024-12-12 14:35:57 +0700

    Fix: CourseListScreen not render course for the first time

--- lib/screens/CourseListScreen.dart
+++ lib/screens/CourseListScreen.dart
@@ -18,17 +18,17 @@ class CourseListScreen extends StatefulWidget {
 
 class _CourseListScreenState extends State<CourseListScreen> {
   late CourseViewModel _courseVM;
-  final storage = new FlutterSecureStorage();
+  final storage = FlutterSecureStorage();
   String? token;
 
-  @override
-  void initState() {
-    super.initState();
-    // Fetch courses asynchronously when the screen initializes
-    _courseVM = Provider.of<CourseViewModel>(context, listen: false);
-    _loadData();
-    _loadToken();
+  Future<void> _loadData() async {
+    try {
+      await _courseVM.getAllCourses();
+    } catch (e) {
+      debugPrint('Error loading courses: $e');
+    }
   }
+
   Future<void> _loadToken() async {
     try {
       token = await storage.read(key: 'token');
@@ -36,20 +36,19 @@ class _CourseListScreenState extends State<CourseListScreen> {
       debugPrint('Error loading token: $e');
     }
   }
-  Future<void> _loadData() async {
-    try {
-      await _courseVM.getAllCourses();
-    } catch (e) {
-      debugPrint('Error loading courses 2: $e');
-    }
+
+  @override
+  void initState() {
+    super.initState();
+    _courseVM = Provider.of<CourseViewModel>(context, listen: false);
+    _loadData();
+    _loadToken();
   }
+
   @override
   Widget build(BuildContext context) {
-
-
     return SafeArea(
-      child:
-      Scaffold(
+      child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
           elevation: 0,
@@ -60,11 +59,10 @@ class _CourseListScreenState extends State<CourseListScreen> {
                 height: 31,
                 child: Image.asset("assets/eduhublogo.png"),
               ),
-              const Spacer(), // Thêm khoảng trống giữa logo và nút đăng nhập
+              const Spacer(),
               Container(
                 height: 40,
-                child:
-                TextButton(
+                child: TextButton(
                   onPressed: () {
                     Navigator.push(
                       context,
@@ -89,94 +87,95 @@ class _CourseListScreenState extends State<CourseListScreen> {
             ],
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 224,
-                child: Image.asset(
-                  "assets/homeimage.jpg",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
+        body: FutureBuilder<void>(
+          future: Future.wait([_loadData(), _loadToken()]), // Wait for both
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error loading data'));
+            } else {
+              return SingleChildScrollView(
                 child: Column(
                   children: [
-                    CourseListCategory(
-                      categoryTitle: 'Lịch học trực tiếp',
-                      courses: _courseVM.courses,
+                    SizedBox(
+                      height: 224,
+                      child: Image.asset("assets/homeimage.jpg"),
                     ),
-                    CourseListCategory(
-                      categoryTitle: 'Lịch học trực tiếp',
-                      courses: _courseVM.courses,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        children: [
+                          CourseListCategory(
+                            categoryTitle: 'Lịch học trực tiếp',
+                            courses: _courseVM.courses,
+                          ),
+                          CourseListCategory(
+                            categoryTitle: 'Lịch học trực tiếp',
+                            courses: _courseVM.courses,
+                          ),
+                          const HomeExploreCategory(),
+                        ],
+                      ),
                     ),
-                    /*CourseListCategory(
-                      categoryTitle: 'Top bán chạy',
-                      courses: homeCourses,
+                    const SizedBox(height: 24),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 24,
+                      ),
+                      decoration: const BoxDecoration(
+                        color: AppColors.success50,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Trở thành giảng viên của EduHub",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: AppColors.success500,
+                            ),
+                          ),
+                          const SizedBox(height: 14),
+                          _buildInstructorInfoSection(context),
+                          const SizedBox(height: 14),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.success700,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 24,
+                                vertical: 12,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: const Text(
+                              "Bắt đầu giảng dạy",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    CourseListCategory(
-                      categoryTitle: 'Khóa học mới ra mắt',
-                      courses: homeCourses,
-                    ),*/
-                    const HomeExploreCategory(),
                   ],
                 ),
-              ),
-              const SizedBox(height: 24),
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 24,
-                ),
-                decoration: const BoxDecoration(
-                  color: AppColors.success50,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Trở thành giảng viên của EduHub",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                        color: AppColors.success500,
-                      ),
-                    ),
-                    const SizedBox(height: 14),
-                    _buildInstructorInfoSection(context),
-                    const SizedBox(height: 14),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.success700,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 24,
-                          vertical: 12,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: const Text(
-                        "Bắt đầu giảng dạy",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+              );
+            }
+          },
         ),
       ),
     );
   }
 }
+
 
 Widget _buildInstructorInfoSection(BuildContext context) {
   return Padding(
