7a4848aa4ecb575ec7043e7047253714fab27c4e
Sang <75106540+sang1833@users.noreply.github.com>
2024-12-12 12:43:17 +0700

    teacher sign up

--- fe-web/src/router/Router.jsx
+++ fe-web/src/router/Router.jsx
@@ -1,34 +1,79 @@
 /* eslint-disable react-refresh/only-export-components */
 import { lazy } from "react";
-import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate
+} from "react-router-dom";
 import withSuspense from "./WithSuspense";
+import SignUpForm from "@/pages/Lecturer/SignUp/SignUpForm";
+import VerifyCodeLecturer from "@/pages/Lecturer/SignUp/VerifyCodeLecturer";
 
 const UserLayout = withSuspense(lazy(() => import("../layouts/UserLayout")));
 const Home = withSuspense(lazy(() => import("../pages/HomePage/Home")));
-const CourseDetail = withSuspense(lazy(() => import("@/pages/CourseDetailPage/CourseDetail")));
+const CourseDetail = withSuspense(
+  lazy(() => import("@/pages/CourseDetailPage/CourseDetail"))
+);
 const SignUp = withSuspense(lazy(() => import("@/pages/SignUp/SignUp")));
 const SignIn = withSuspense(lazy(() => import("@/pages/SignIn/SignIn")));
 const StepOne = withSuspense(lazy(() => import("@/pages/SignUp/StepOne")));
-const VerifyCode = withSuspense(lazy(() => import("@/pages/SignUp/VerifyCode")));
-const ResultPage = withSuspense(lazy(() => import("@/pages/Result/ResultPage")));
+const VerifyCode = withSuspense(
+  lazy(() => import("@/pages/SignUp/VerifyCode"))
+);
+const ResultPage = withSuspense(
+  lazy(() => import("@/pages/Result/ResultPage"))
+);
 const InfoInput = withSuspense(lazy(() => import("@/pages/SignUp/InfoInput")));
-const LecturerLayout = withSuspense(lazy(() => import("@/layouts/LecturerLayout")));
-const LecturerSignUp = withSuspense(lazy(() => import("@/pages/Lecturer/SignUp/LecturerSignUp")));
-const LecturerResultPage = withSuspense(lazy(() => import("@/pages/Lecturer/Result/ResultPage")));
-const LecturerLandingPage = withSuspense(lazy(() => import("@/pages/Lecturer/LandingPage/LecturerLandingPage.jsx")));
-const TeacherCoursePage = withSuspense(lazy(() => import("@/pages/Lecturer/TeacherCoursePage/TeacherCoursePage")));
-const LecturerCourseDetail = withSuspense(lazy(() => import("@/pages/Lecturer/CourseDetail/LecturerCourseDetail")));
-const CourseAdd = withSuspense(lazy(() => import("@/pages/Lecturer/TeacherCoursePage/AddCourse/CourseAdd")));
-const SignInForm = withSuspense(lazy(() => import("@/pages/SignIn/SignInForm")));
-const ForgotPassword = withSuspense(lazy(() => import("@/pages/SignIn/ForgotPassword")));
-const ResetPassword = withSuspense(lazy(() => import("@/pages/SignIn/ResetPassword")));
+const LecturerLayout = withSuspense(
+  lazy(() => import("@/layouts/LecturerLayout"))
+);
+const LecturerSignUp = withSuspense(
+  lazy(() => import("@/pages/Lecturer/SignUp/LecturerSignUp"))
+);
+const LecturerResultPage = withSuspense(
+  lazy(() => import("@/pages/Lecturer/Result/ResultPage"))
+);
+const LecturerLandingPage = withSuspense(
+  lazy(() => import("@/pages/Lecturer/LandingPage/LecturerLandingPage.jsx"))
+);
+const TeacherCoursePage = withSuspense(
+  lazy(() => import("@/pages/Lecturer/TeacherCoursePage/TeacherCoursePage"))
+);
+const LecturerCourseDetail = withSuspense(
+  lazy(() => import("@/pages/Lecturer/CourseDetail/LecturerCourseDetail"))
+);
+const CourseAdd = withSuspense(
+  lazy(() => import("@/pages/Lecturer/TeacherCoursePage/AddCourse/CourseAdd"))
+);
+const SignInForm = withSuspense(
+  lazy(() => import("@/pages/SignIn/SignInForm"))
+);
+const ForgotPassword = withSuspense(
+  lazy(() => import("@/pages/SignIn/ForgotPassword"))
+);
+const ResetPassword = withSuspense(
+  lazy(() => import("@/pages/SignIn/ResetPassword"))
+);
 const NotFound = withSuspense(lazy(() => import("@/pages/NotFound")));
-const SearchPage = withSuspense(lazy(() => import("@/pages/searchPage/searchPage")));
-const DefaultLayout = withSuspense(lazy(() => import("@/layouts/DefaultLayout")));
-const LecturerSignIn = withSuspense(lazy(() => import("@/pages/Lecturer/SignIn/LecturerSignIn")));
-const CartPage = withSuspense(lazy(() => import("@/pages/CartPage/CartPage.jsx")));
-const CourseList = withSuspense(lazy(() => import("@/pages/CourseList/CourseList.jsx")));
-const LearningPage = withSuspense(lazy(() => import("@/pages/LearningPage/LearningPage.jsx")));
+const SearchPage = withSuspense(
+  lazy(() => import("@/pages/searchPage/searchPage"))
+);
+const DefaultLayout = withSuspense(
+  lazy(() => import("@/layouts/DefaultLayout"))
+);
+const LecturerSignIn = withSuspense(
+  lazy(() => import("@/pages/Lecturer/SignIn/LecturerSignIn"))
+);
+const CartPage = withSuspense(
+  lazy(() => import("@/pages/CartPage/CartPage.jsx"))
+);
+const CourseList = withSuspense(
+  lazy(() => import("@/pages/CourseList/CourseList.jsx"))
+);
+const LearningPage = withSuspense(
+  lazy(() => import("@/pages/LearningPage/LearningPage.jsx"))
+);
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
@@ -37,16 +82,28 @@ const router = createBrowserRouter(
           <Route index element={<Home />} />
           <Route path="course/:id" element={<CourseDetail />} />
           <Route path="sign-up" element={<SignUp />}>
-            <Route path="*" element={<Navigate to={"/web/sign-up/step1/email"} />} />
+            <Route
+              path="*"
+              element={<Navigate to={"/web/sign-up/step1/email"} />}
+            />
             <Route path="step1/:signUpType" element={<StepOne />} />
-            <Route path="step2/:signUpType/:emailOrPhone" element={<VerifyCode />} />
+            <Route
+              path="step2/:signUpType/:emailOrPhone"
+              element={<VerifyCode />}
+            />
           </Route>
           <Route path="sign-in" element={<SignIn />}>
             <Route path="*" element={<Navigate to={"/web/sign-in"} />} />
             <Route index element={<SignInForm />} />
             <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="step2/:signUpType/:emailOrPhone" element={<VerifyCode />} />
-            <Route path="reset-password/:emailOrPhone/:otp" element={<ResetPassword />} />
+            <Route
+              path="step2/:signUpType/:emailOrPhone"
+              element={<VerifyCode />}
+            />
+            <Route
+              path="reset-password/:emailOrPhone/:otp"
+              element={<ResetPassword />}
+            />
           </Route>
           <Route path="search" element={<SearchPage />} />
           <Route path="callback" element={<InfoInput />} />
@@ -56,7 +113,17 @@ const router = createBrowserRouter(
           {/* Lecturer layout */}
           <Route path="lecturer" element={<LecturerLayout />}>
             <Route path="sign-in" element={<LecturerSignIn />} />
-            <Route path="sign-up" element={<LecturerSignUp />} />
+            <Route path="sign-up" element={<LecturerSignUp />}>
+              <Route
+                path="*"
+                element={<Navigate to={"/web/lecturer/sign-up"} />}
+              />
+              <Route index element={<SignUpForm />} />
+              <Route
+                path="step2/:signUpType/:emailOrPhone"
+                element={<VerifyCodeLecturer />}
+              />
+            </Route>
             <Route path="result" element={<LecturerResultPage />} />
             <Route path="landing" element={<LecturerLandingPage />} />
             <Route path="course" element={<TeacherCoursePage />} />
@@ -75,8 +142,8 @@ const router = createBrowserRouter(
       v7_fetcherPersist: true,
       v7_normalizeFormMethod: true,
       v7_partialHydration: true,
-      v7_skipActionStatusRevalidation: true,
-    },
+      v7_skipActionStatusRevalidation: true
+    }
   }
 );
 
--- fe-web/src/store/store.js
+++ fe-web/src/store/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./slices/authSlice";
 import learning from "./slices/learningSlice";
+import signUp from "./slices/signUpSlice";
 import { courseRTKApi } from "./rtk/course.services";
+
 export default configureStore({
   reducer: {
     auth: auth,
     learning: learning,
-    [courseRTKApi.reducerPath]: courseRTKApi.reducer,
+    signUp: signUp,
+    [courseRTKApi.reducerPath]: courseRTKApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(courseRTKApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(courseRTKApi.middleware)
 });
