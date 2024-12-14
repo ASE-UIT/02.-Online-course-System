a77445c0b2c6e654006dde70ff9290fecb3427ad
chabuuuu <haphuthinh332004@gmail.com>
2024-12-15 00:22:57 +0700

    feat(order api): create with order ids

--- main/src/container/order.container.ts
+++ main/src/container/order.container.ts
@@ -11,6 +11,8 @@ import { ICartRepository } from '@/repository/interface/i.cart.repository';
 import { cartRepository } from '@/container/cart.container';
 import { IEnrollmentRepository } from '@/repository/interface/i.enrollment.repository';
 import { enrollmentRepository } from '@/container/enrollment.container';
+import { ICourseRepository } from '@/repository/interface/i.course.repository';
+import { courseRepository } from '@/container/course.container';
 
 class OrderContainer extends BaseContainer {
   constructor() {
@@ -23,6 +25,7 @@ class OrderContainer extends BaseContainer {
     this.container.bind<IDiscountRepository<any>>('DiscountRepository').toConstantValue(discountRepository);
     this.container.bind<ICartRepository<any>>('CartRepository').toConstantValue(cartRepository);
     this.container.bind<IEnrollmentRepository<any>>('EnrollmentRepository').toConstantValue(enrollmentRepository);
+    this.container.bind<ICourseRepository<any>>('CourseRepository').toConstantValue(courseRepository);
   }
 
   export() {
