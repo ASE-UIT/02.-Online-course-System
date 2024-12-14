b91b120aa8000cb46bd8ef0ea84a2923dc9f88de
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2024-12-14 22:13:39 +0700

    feat(statistical): get week, month, year statistical

--- main/src/repository/interface/i.statistical.repository.ts
+++ main/src/repository/interface/i.statistical.repository.ts
@@ -1,1 +1,18 @@
-export interface IStatisticalRepository {}
+export interface IStatisticalRepository {
+  getStatisticsBetweenDates(time: number): Promise<{
+    revenue: number;
+    revenuePercentage: number;
+    newStudents: number;
+    newStudentsPercentage: number;
+    newLecturer: number;
+    newLecturerPercentage: number;
+    coursesPurchased: number;
+    coursesPurchasedPercentage: number;
+    newCourses: number;
+    newCoursesPercentage: number;
+    courseRatings: number;
+    courseRatingsPercentage: number;
+    averageRating: number;
+    averageRatingPercentage: number;
+  }>;
+}
