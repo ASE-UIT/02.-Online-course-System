d29ed0538ebac26c72a02e53cfe8d4cb9ec205c8
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2024-12-18 15:53:27 +0700

    map api lecturer ad

--- fe-admin/src/components/Dialog/Lecturer/LecturerModalBody.jsx
+++ fe-admin/src/components/Dialog/Lecturer/LecturerModalBody.jsx
@@ -1,7 +1,6 @@
+import { createLecturer, updateLecturer } from "@/api/employeeApi";
 import { mediaApi } from "@/api/mediaApi";
 import { Button } from "@/components/ui/button";
-import { useCreateCategoryMutation } from "@/store/rtk/category.service";
-import { useUpdateLecturerMutation } from "@/store/rtk/lecturer.service";
 import { UploadCloud } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import { z } from "zod";
@@ -11,34 +10,37 @@ const lecturerSchema = z.object({
   name: z.string().min(1, "Tên không được để trống."),
   title: z.string().optional(),
   email: z.string().email("Email không hợp lệ."),
-  phone: z.string().optional(),
+  phoneNumber: z.string().optional(),
   password: z.string().min(6, "Mật khẩu phải có ít nhất 6 ký tự."),
-  avatar: z.custom((file) => file instanceof File && file.type.startsWith("image/"), {
-    message: "Vui lòng tải lên một tệp hình ảnh hợp lệ.",
-  }),
+  // avatar: z.custom((file) => file instanceof File && file.type.startsWith("image/"), {
+  //   message: "Vui lòng tải lên một tệp hình ảnh hợp lệ.",
+  // }),
+  // address: z.string().min(1, "Địa chỉ không được để trống."),
 });
 const LecturerModalBody = ({ row, isAddOrChange }) => {
   const fileInputRef = useRef(null);
   const [fileSlt, setFileSlt] = useState(null);
   const [lecturerName, setLecturerName] = useState("");
-  const [lecturerTitle, setLecturerTitle] = useState("");
+  // const [lecturerTitle, setLecturerTitle] = useState("");
   const [lecturerEmail, setLecturerEmail] = useState("");
   const [lecturerPhone, setLecturerPhone] = useState("");
+  const [lecturerAddress, setLecturerAddress] = useState("");
   const [lecturerPassword, setLecturerPassword] = useState("");
   const [lecturerAvatar, setLecturerAvatar] = useState(null);
   const [formErrors, setFormErrors] = useState({});
 
-  const [createLecturer] = useCreateCategoryMutation();
-  const [updateLecturer] = useUpdateLecturerMutation();
+  // const [createLecturer] = useCreateCategoryMutation();
+  // const [updateLecturer] = useUpdateLecturerMutation();
 
   useEffect(() => {
     if (!isAddOrChange && row) {
       setLecturerName(row.name || "");
-      setLecturerTitle(row.title || "");
+      // setLecturerTitle(row.title || "");
       setLecturerEmail(row.email || "");
-      setLecturerPhone(row.phone || "");
+      setLecturerPhone(row.phoneNumber || "");
       setLecturerAvatar(row.avatar || null);
       setLecturerPassword(row.password || "");
+      setLecturerAddress(row.address || "");
     }
   }, [isAddOrChange, row]);
 
@@ -61,21 +63,25 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
 
   const handleFormSubmit = async () => {
     setFormErrors({});
-    if (!fileSlt && !lecturerAvatar) {
-      setFormErrors((prevErrors) => ({
-        ...prevErrors,
-        avatar: "Ảnh đại diện là bắt buộc.",
-      }));
-      return;
-    }
+
+    // if (!fileSlt && !lecturerAvatar) {
+    //   setFormErrors((prevErrors) => ({
+    //     ...prevErrors,
+    //     avatar: "Ảnh đại diện là bắt buộc.",
+    //   }));
+    //   console.log("Avatar error: Missing file or existing avatar.");
+    //   return;
+    // }
     const payload = {
       name: lecturerName,
-      title: lecturerTitle,
+      // title: lecturerTitle,
       email: lecturerEmail,
-      phone: lecturerPhone,
+      phoneNumber: lecturerPhone,
       password: lecturerPassword,
       avatar: fileSlt || lecturerAvatar,
+      address: lecturerAddress,
     };
+    console.log("Payload before validation:", payload);
     const baseValidation = lecturerSchema.safeParse(payload);
 
     if (!baseValidation.success) {
@@ -84,6 +90,7 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
         errors[err.path[0]] = err.message;
       });
       setFormErrors(errors);
+      console.log("Validation errors:", errors);
       return;
     }
     try {
@@ -98,10 +105,10 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
       };
       console.log("Final Payload:", finalPayload);
       if (isAddOrChange) {
-        await createLecturer(finalPayload).unwrap();
+        await createLecturer(finalPayload);
         alert("Lecturer created successfully!");
       } else {
-        await updateLecturer({ id: row.id, ...finalPayload }).unwrap();
+        await updateLecturer(row.id, finalPayload);
         alert("Lecturer updated successfully!");
       }
     } catch (error) {
@@ -122,7 +129,7 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
           {fileSlt ? (
             <p className="text-sm text-green-600">{fileSlt.name}</p>
           ) : lecturerAvatar ? (
-            <img src={"lecturerAvatar"} alt="Current Avatar" className="w-24 h-24 object-cover rounded-md" />
+            <img src={lecturerAvatar} alt="Current Avatar" className="w-24 h-24 object-cover rounded-md" />
           ) : (
             <p className="text-sm text-red-500">Chưa có ảnh đại diện.</p>
           )}
@@ -147,35 +154,27 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
           <label className="block text-md font-medium mb-2">
             Tên<span className="text-red-500">*</span>
           </label>
-          {isAddOrChange ? (
-            <input
-              type="text"
-              className={`w-full border ${formErrors.name ? "border-red-500" : "border-gray-600"} rounded-md px-3 py-2`}
-              placeholder="Enter name"
-              value={lecturerName}
-              onChange={(e) => setLecturerName(e.target.value)}
-            />
-          ) : (
-            <p className="text-lg font-semibold">{lecturerName}</p>
-          )}
+          <input
+            type="text"
+            className={`w-full border ${formErrors.name ? "border-red-500" : "border-gray-600"} rounded-md px-3 py-2`}
+            placeholder="Enter name"
+            value={lecturerName}
+            onChange={(e) => setLecturerName(e.target.value)}
+          />
           {formErrors.name && <p className="text-sm text-red-500 mt-1">{formErrors.name}</p>}
         </div>
 
         {/* Title */}
-        <div className="title">
+        {/* <div className="title">
           <label className="block text-md font-medium mb-2">Chức danh</label>
-          {isAddOrChange ? (
-            <input
-              type="text"
-              className="w-full border border-gray-600 rounded-md px-3 py-2"
-              placeholder="Enter title"
-              value={lecturerTitle}
-              onChange={(e) => setLecturerTitle(e.target.value)}
-            />
-          ) : (
-            <p className="text-lg font-semibold">{lecturerTitle}</p>
-          )}
-        </div>
+          <input
+            type="text"
+            className="w-full border border-gray-600 rounded-md px-3 py-2"
+            placeholder="Enter title"
+            value={lecturerTitle}
+            onChange={(e) => setLecturerTitle(e.target.value)}
+          />
+        </div> */}
 
         {/* Email */}
         <div className="email">
@@ -213,25 +212,40 @@ const LecturerModalBody = ({ row, isAddOrChange }) => {
             <p className="text-lg font-semibold">{lecturerPhone}</p>
           )}
         </div>
-
-        {/* Password */}
-        {isAddOrChange && (
-          <div className="password">
-            <label className="block text-md font-medium mb-2">
-              Mật khẩu<span className="text-red-500">*</span>
-            </label>
+        {/* Address */}
+        <div className="address">
+          <label className="block text-md font-medium mb-2">Địa chỉ</label>
+          {isAddOrChange ? (
             <input
-              type="password"
+              type="text"
               className={`w-full border ${
-                formErrors.password ? "border-red-500" : "border-gray-600"
+                formErrors.address ? "border-red-500" : "border-gray-600"
               } rounded-md px-3 py-2`}
-              placeholder="Enter password"
-              value={lecturerPassword}
-              onChange={(e) => setLecturerPassword(e.target.value)}
+              placeholder="Enter address"
+              value={lecturerAddress}
+              onChange={(e) => setLecturerAddress(e.target.value)}
             />
-            {formErrors.password && <p className="text-sm text-red-500 mt-1">{formErrors.password}</p>}
-          </div>
-        )}
+          ) : (
+            <p className="text-lg font-semibold">{lecturerAddress}</p>
+          )}
+          {formErrors.address && <p className="text-sm text-red-500 mt-1">{formErrors.address}</p>}
+        </div>
+        {/* Password */}
+        <div className="password">
+          <label className="block text-md font-medium mb-2">
+            Mật khẩu<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="password"
+            className={`w-full border ${
+              formErrors.password ? "border-red-500" : "border-gray-600"
+            } rounded-md px-3 py-2`}
+            placeholder="Enter password"
+            value={lecturerPassword}
+            onChange={(e) => setLecturerPassword(e.target.value)}
+          />
+          {formErrors.password && <p className="text-sm text-red-500 mt-1">{formErrors.password}</p>}
+        </div>
 
         {/* Buttons */}
         <div className="buttons flex justify-end gap-[10px]">
